98a52e1831d6b3201e33bc33c4f40462c3c39406
mamsaka <zefir4ick2008@gmail.com>
2025-11-24 15:06:06 +0500

    Auto-update 24.11.2025 15:06:05,91

--- IPStatusListener/IPStatusListener/Form1.Designer.cs
+++ IPStatusListener/IPStatusListener/Form1.Designer.cs
@@ -28,13 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.IPList = new System.Windows.Forms.ListBox();
+            this.enterIP = new System.Windows.Forms.TextBox();
+            this.saveIP = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // IPList
+            // 
+            this.IPList.FormattingEnabled = true;
+            this.IPList.ItemHeight = 16;
+            this.IPList.Location = new System.Drawing.Point(424, 12);
+            this.IPList.Name = "IPList";
+            this.IPList.Size = new System.Drawing.Size(364, 420);
+            this.IPList.TabIndex = 0;
+            this.IPList.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.IPList_MouseDoubleClick);
+            // 
+            // enterIP
+            // 
+            this.enterIP.Location = new System.Drawing.Point(12, 12);
+            this.enterIP.Name = "enterIP";
+            this.enterIP.Size = new System.Drawing.Size(300, 22);
+            this.enterIP.TabIndex = 3;
+            // 
+            // saveIP
+            // 
+            this.saveIP.Location = new System.Drawing.Point(318, 12);
+            this.saveIP.Name = "saveIP";
+            this.saveIP.Size = new System.Drawing.Size(100, 30);
+            this.saveIP.TabIndex = 2;
+            this.saveIP.Text = "Save";
+            this.saveIP.UseVisualStyleBackColor = true;
+            this.saveIP.Click += new System.EventHandler(this.saveIP_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.enterIP);
+            this.Controls.Add(this.saveIP);
+            this.Controls.Add(this.IPList);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox IPList;
+        private System.Windows.Forms.TextBox enterIP;
+        private System.Windows.Forms.Button saveIP;
     }
 }
 
--- IPStatusListener/IPStatusListener/Form1.cs
+++ IPStatusListener/IPStatusListener/Form1.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +14,128 @@ namespace IPStatusListener
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            await MonitorServers(); // запуск мониторинга
+        }
+
+        private void saveIP_Click(object sender, EventArgs e)
+        {
+            string text = enterIP.Text.Trim();
+
+            if (string.IsNullOrEmpty(text))
+            {
+                MessageBox.Show("Поле ввода пустое!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (CheckType(text) == "Unknown")
+            {
+                MessageBox.Show("Невалидный ввод!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Добавляем в ListBox
+            IPList.Items.Add(text);
+
+            enterIP.Clear();
+            enterIP.Focus();
+        }
+
+        private void IPList_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (IPList.SelectedIndex == -1)
+                return;
+
+            string item = IPList.SelectedItem.ToString();
+
+            if (MessageBox.Show($"Удалить элемент: \"{item}\"?",
+                "Удаление",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                IPList.Items.RemoveAt(IPList.SelectedIndex);
+            }
+        }
+
+        // -----------------------
+        // Проверка IP или домена
+        // -----------------------
+
+        static string CheckType(string input)
+        {
+            if (IPAddress.TryParse(input, out _))
+                return "IP";
+
+            if (IsValidDomain(input))
+                return "Domain";
+
+            return "Unknown";
+        }
+
+        static bool IsValidDomain(string input)
+        {
+            input = input.Replace("http://", "").Replace("https://", "");
+
+            string pattern = @"^(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}$";
+            return Regex.IsMatch(input, pattern);
+        }
+
+        // -----------------------
+        // Проверка статуса
+        // -----------------------
+
+        private string CheckServerStatus(string address)
+        {
+            try
+            {
+                Ping ping = new Ping();
+                PingReply reply = ping.Send(address, 1000);
+
+                if (reply.Status == IPStatus.Success)
+                    return "online server";
+
+                return "offline server";
+            }
+            catch
+            {
+                return "unable to ping";
+            }
+        }
+
+        // -----------------------
+        // Мониторинг серверов
+        // -----------------------
+
+        private async Task MonitorServers()
+        {
+            while (true)
+            {
+                // Берём копию, чтобы не падало при удалении элементов во время цикла
+                List<string> items = IPList.Items.Cast<string>().ToList();
+
+                foreach (var item in items)
+                {
+                    string IP = item
+                        .Replace(": online server", "")
+                        .Replace(": offline server", "")
+                        .Replace(": unable to ping", "");
+
+                    string status = await Task.Run(() => CheckServerStatus(IP));
+                    string newStatus = $"{IP}: {status}";
+
+                    Invoke(new Action(() =>
+                    {
+                        int index = IPList.Items.IndexOf(item);
+                        if (index != -1)
+                            IPList.Items[index] = newStatus;
+                    }));
+                }
+
+                await Task.Delay(1000);
+            }
         }
     }
 }
