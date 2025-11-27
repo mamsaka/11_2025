d8c59b499edfe2d84e454a4e307052b6f88dd67a
mamsaka <zefir4ick2008@gmail.com>
2025-11-27 15:31:44 +0500

    Auto-update 27.11.2025 15:31:43,86

--- CheckListCreator/CheckListCreator/obj/Debug/net10.0-windows/ChecklistEditor.g.i.cs
+++ CheckListCreator/CheckListCreator/obj/Debug/net10.0-windows/ChecklistEditor.g.i.cs
@@ -116,7 +116,7 @@ namespace CheckListCreator {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/CheckListCreator;V1.0.0.0;component/checklisteditor.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/CheckListCreator;component/checklisteditor.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\ChecklistEditor.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
