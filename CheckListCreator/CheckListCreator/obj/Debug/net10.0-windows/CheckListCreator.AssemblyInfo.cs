15e2553146d30acc1b3a49845d79db3e287f7819
mamsaka <zefir4ick2008@gmail.com>
2025-12-05 10:38:54 +0500

    Auto-update 05.12.2025 10:38:54,33

--- CheckListCreator/CheckListCreator/obj/Debug/net10.0-windows/CheckListCreator.AssemblyInfo.cs
+++ CheckListCreator/CheckListCreator/obj/Debug/net10.0-windows/CheckListCreator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CheckListCreator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a98fa83a59978731dbe553403cc419ff56f10c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+449968ec1ecc98e96919c0ce7a0304306a17fd90")]
 [assembly: System.Reflection.AssemblyProductAttribute("CheckListCreator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CheckListCreator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
